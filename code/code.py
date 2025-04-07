438edd1798bc40185d1583bf37af2f715234de3f
m-modak <sarahm046@stemcollegiate.ca>
2025-04-07 12:33:24 -0600

    Update and rename code.md to code.py

---
+++ code/code.py
@@ -0,0 +1,25 @@
+myVariable = 0
+
+def when_started1():
+    global myVariable
+    drivetrain.set_drive_velocity(45, PERCENT)
+    # this goes off the ramp and over the bumps
+    drivetrain.drive_for(FORWARD, 34, INCHES)
+    # the first set of turns
+    drivetrain.turn_for(LEFT, 90, DEGREES)
+    drivetrain.drive_for(FORWARD, 12, INCHES)
+    drivetrain.turn_for(LEFT, 90, DEGREES)
+    # going over the ramp and around the blocks
+    drivetrain.drive_for(FORWARD, 12, INCHES)
+    drivetrain.turn_for(LEFT, 45, DEGREES)
+    drivetrain.drive_for(FORWARD, 10, INCHES)
+    drivetrain.turn_for(RIGHT, 90, DEGREES)
+    drivetrain.drive_for(FORWARD, 10, INCHES)
+    # next set of turns
+    drivetrain.turn_for(RIGHT, 45, DEGREES)
+    drivetrain.drive_for(FORWARD, 12, MM)
+    drivetrain.turn_for(RIGHT, 90, DEGREES)
+    # going through the wall and to the ball
+    drivetrain.drive_for(FORWARD, 36, INCHES)
+
+when_started1()
